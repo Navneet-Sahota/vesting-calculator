fdca0f498b121fd8859c0193d946f328ba14395a
Navneet Sahota <navneet.sahota@outlook.com>
2020-01-15 15:07:33 +0530

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header as="h1" className="center">
+        Vesting Calculator
+      </Header>
+    </Container>
   );
 }
 
